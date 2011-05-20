a599a558e7416e0d0c36887d7664ac9b7862bbf2
Ian Murray <ianmurrays@gmail.com>
2011-05-20 10:43:54 -0400

    Initial commit, Rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Juggerchat::Application.config.secret_token = '32202435771748b98347f9ca3e6fd7ab67489eac2a557e5fa9f56815f65a346a15f2453509469a5601bdb9f4888e15d53d464c2a8ca5e3cfc7e9dfb2f824f00b'
